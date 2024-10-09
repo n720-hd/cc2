5cda5ccfc01a397a50b9bb85b47a9a3f643d104a
n720-hd <devandjayakarta@Devans-MacBook-Pro.local>
2024-10-09 16:48:41 +0700

    update

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -27,13 +27,14 @@ export default function Navbar(){
                     src={commerzbanklogo}
                     height={50}
                     width={50}
+                    alt="bank-logo"
                 />
-                <a className="btn btn-ghost text-xl">Commerzbank</a>
+                <a name='button' className="btn btn-ghost text-xl">Commerzbank</a>
             </div>
                 
     <div className="navbar-end">
             <CiLock className="text-2xl"/>
-        <button className="btn btn-ghost btn-circle">
+        <button name='button' className="btn btn-ghost btn-circle">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -76,27 +77,3 @@ export default function Navbar(){
     )
 
 } 
-
-            // <div className="dropdown">
-            //     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            //         <svg
-            //         xmlns="http://www.w3.org/2000/svg"
-            //         className="h-5 w-5"
-            //         fill="none"
-            //         viewBox="0 0 24 24"
-            //         stroke="currentColor">
-            //         <path
-            //             strokeLinecap="round"
-            //             strokeLinejoin="round"
-            //             strokeWidth="2"
-            //             d="M4 6h16M4 12h16M4 18h7" />
-            //         </svg>
-            //     </div>
-            //     <ul
-            //         tabIndex={0}
-            //         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            //         <li><a>Homepage</a></li>
-            //         <li><a>Portfolio</a></li>
-            //         <li><a>About</a></li>
-            //     </ul>
-            // </div>
